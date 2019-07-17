9ea9ad04669e36431b5757d3540263461c12e35f
Erisvan Junior <erisvan.junior.a@gmail.com>
2019-07-17 02:48:31 -0300

    Config Firebase and CI

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,11 @@ import React from 'react'
 import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom'
 
 import Home from './pages/Home'
-import SignIn from './pages/SignIn'
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path='/' exact component={Home} />
-      <Route path='/signin' component={SignIn} />
       <Redirect from='*' to='/' />
     </Switch>
   </BrowserRouter>
